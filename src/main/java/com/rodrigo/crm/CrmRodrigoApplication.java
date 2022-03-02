6dc831ff940e93fb66804d95c0411b0d6d59e1cc
MacBook Pro de Rodrigo <macbookpro@MacBook-Pro-de-MacBook.local>
2022-03-01 23:26:15 -0400

    teste

---
+++ src/main/java/com/rodrigo/crm/CrmRodrigoApplication.java
@@ -0,0 +1,13 @@
+package com.rodrigo.crm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrmRodrigoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrmRodrigoApplication.class, args);
+	}
+
+}
